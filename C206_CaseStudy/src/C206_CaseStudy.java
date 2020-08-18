0862b4c57123221acf306e017260be3c1fc5b9df
19030130 <19030130@192.168.184.1>
2020-08-18 23:33:23 +0800

    Package(Fatheen) method and Appointment method added

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-//import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatter;
 import java.time.LocalTime;
 
 public class C206_CaseStudy {
@@ -23,8 +23,11 @@ public class C206_CaseStudy {
 
 		appointmentList.add(new Appointment("Jason", dateNow, timeNow, "Jerome", "Woodlands"));
 
-		int option = Helper.readInt("Enter an option > ");
+		// Fatheen
+				ArrayList<Package> packageList = new ArrayList<Package>();
+				packageList.add(new Package("WTCS", "For Rain", "14/03/2020", "10/04/2020", "42.20"));
 		menu();
+		int option = Helper.readInt("Enter an option > ");
 
 		// Syakir
 		while (option != 6) {
@@ -35,17 +38,11 @@ public class C206_CaseStudy {
 
 			} else if (option == 3) {
 				// Syakir
-				System.out.println("1. Add an Appointment");
-				System.out.println("2. View all Appointment");
-				System.out.println("3. Delete and Appointment");
-
-				int apptOption = Helper.readInt("Enter an option > ");
-
-				if (apptOption == 1) {
-
-				}
-
+				Appointment(appointmentList);
+				
 			} else if (option == 4) {
+				//Fatheen
+				ManagePackage(packageList);
 
 			} else if (option == 5) {
 
@@ -74,6 +71,7 @@ public class C206_CaseStudy {
 	/**
 	 * @param string
 	 */
+	//Syakir
 	private static void setHeader(String header) {
 		Helper.line(80, "-");
 		System.out.println(header);
@@ -137,18 +135,158 @@ public class C206_CaseStudy {
 
 	// Syakir
 	public static Appointment inputAppointment() {
-
 		String name = Helper.readString("Enter name: ");
-		Date date = Helper.readDate("Enter date: ");
+		DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("M/d/yyyy");
+		String inputDate = Helper.readString("Enter date in (M/d/yyyy) format : ");
+		LocalDate date = LocalDate.parse(inputDate, formatter1);
+		DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("HH:mm");
+		String inputTime = Helper.readString("Enter time in (HH:mm) format : ");
+		LocalDateTime time = LocalDateTime.parse(inputTime, formatter2);
+		String designerName = Helper.readString("Enter designer name: ");
+		String address = Helper.readString("Enter address");
 
-		return null;
+		Appointment appt = new Appointment(name, date, time, designerName, address);
 
+		return appt;
+		
 	}
 
 	// Syakir
-	private static void Appointment(ArrayList<ArrayList> appointmentList) {
-		C206_CaseStudy.setHeader("APPOINTMENT LIST");
+	private static void addAppointment(ArrayList<Appointment> appointmentList, Appointment appt) {
+		
+		appointmentList.add(appt);
+		System.out.println("Appointment added");
 
+	} //Syakir
+	private static void Appointment(ArrayList<Appointment> appointmentList) {
+		C206_CaseStudy.setHeader(
+				"APPOINTMENT LIST \n1. Add appointment \n2. View appointment \n3. Delete appointment\n4. Back to menu");
+		int option = Helper.readInt("Enter an option > ");
+
+		while (option != 4) {
+
+			if (option == 1) {
+				System.out.println("ADD APPOINTMENT");
+
+				// C206_CaseStudy.addAppointment(appointmentList, appt);
+				String name = Helper.readString("Enter name: ");
+
+				DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("M/d/yyyy");
+				String inputDate = Helper.readString("Enter date in (M/d/yyyy) format : ");
+				LocalDate date = LocalDate.parse(inputDate, formatter1);
+
+				DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("HH:mm");
+				String inputTime = Helper.readString("Enter time in (HH:mm) format : ");
+				LocalDateTime time = LocalDateTime.parse(inputTime, formatter2);
+
+				String designerName = Helper.readString("Enter designer name: ");
+				String address = Helper.readString("Enter address");
+
+				Appointment appt = new Appointment(name, date, time, designerName, address);
+				System.out.println("Appointment added successfully");
+
+			} else if (option == 2) {
+
+				System.out.println("VIEW ALL APPOINTMENT");
+				Helper.line(80, "-");
+
+				for (int i = 0; i < appointmentList.size(); i++) {
+					appointmentList.get(i).showApptDetails();
+					System.out.println();
+
+				}
+
+			} else if (option == 3) {
+				System.out.println("DELETE APPOINTMENT");
+				Helper.line(80, "-");
+				String delAppt = Helper.readString("Enter designer name:");
+				boolean isDeleted = false;
+
+				for (int i = 0; i < appointmentList.size(); i++) {
+					if (isDeleted == false && delAppt.equalsIgnoreCase(appointmentList.get(i).getCustomerName())) {
+						appointmentList.remove(i);
+						isDeleted = true;
+					}
+				}
+				if (isDeleted == true) {
+					System.out.println(
+							String.format("Appointment details of %s has been successfully deleted.", delAppt));
+				} else {
+					System.out.println(String.format("%s has not yet booked an appointment", delAppt));
+				}
+
+			} else {
+				System.out.println("'Invalid option");
+				option = Helper.readInt("Enter an option > ");
+			}
+			System.out.println(
+					"APPOINTMENT LIST \n1. Add appointment \n2.View appointment \n3. Delete appointment\n4. Back to menu");
+			option = Helper.readInt("Enter an option > ");
+		}
 	}
+
+//	// Fatheen
+//	private String code;
+//	private String description;
+//	private LocalDate startDate;
+//	private LocalDate endDate;
+//	private double amount;
+
+	private static void ManagePackage(ArrayList<Package> packageList) {
+		Helper.line(90, "-");
+		System.out.println("PACKAGE \n1. Add Package \n2.View Package \n3. Delete package\n4. Back to menu");
+		Helper.line(90, "-");
+		int option = Helper.readInt("Enter an option > ");
+
+		while (option != 4) {
+			if (option == 1) {
+				Helper.line(80, "-");
+				System.out.println("Add Package");
+				Helper.line(80, "-");
+				String code = Helper.readString("Enter package code > ");
+				String desc = Helper.readString("Enter packagedescription > ");
+				String start = Helper.readString("Enter start date in (DD/MM/YYYY) > ");
+				String end = Helper.readString("Enter end date in (DD/MM/YYYY) > ");
+				String amount = Helper.readString("Enter price > ");
+
+				packageList.add(new Package(code, desc, start, end, amount));
+				System.out.println("Package added successfully");
+			} else if (option == 2) {
+				System.out.println("VIEW ALL USERS");
+				Helper.line(80, "-");
+				String output = String.format("%-5S %-15S %-10S %-10S %-10S \n", "CODE", "DESC", "START", "END",
+						"AMOUNT");
+				for (int i = 0; i < packageList.size(); i++) {
+					output += String.format("%-5S %-15S %-10S %-10S %-10S", packageList.get(i).getCode(),
+							packageList.get(i).getDescription(), packageList.get(i).getStartDate(),
+							packageList.get(i).getEndDate(), packageList.get(i).getAmount());
+				}
+				System.out.println(output);
+			} else if (option == 3) {
+				System.out.println("DELETE PACKAGE BY CODE");
+				Helper.line(80, "-");
+				String delPack = Helper.readString("Enter package code to delete > ");
+				boolean isDeleted = false;
+				for (int i = 0; i < packageList.size(); i++) {
+					if (isDeleted == false && delPack.equalsIgnoreCase(packageList.get(i).getCode())) {
+						packageList.remove(i);
+						isDeleted = true;
+					}
+				}
+				if (isDeleted == true) {
+					System.out.println(
+							String.format("Package and details of %s has been successfully deleted!", delPack));
+				} else {
+					System.out.println(String.format("%s is not registered to the system", delPack));
+				}
+			} else {
+				System.out.println("Invalid option");
+				break;
+			}
+			System.out.println("PACKAGE \n1. Add Package \n2.View Package \n3. Delete package\n4. Back to menu");
+			option = Helper.readInt("Enter an option > ");
+		}
+	}
+
 
 }
