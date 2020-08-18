36f9831f09eb1f2e98af2c602d95d6907872a038
19030130 <19030130@DESKTOP-483ETFT>
2020-08-18 13:06:39 +0800

    Updated Appointment Class

--- C206_CaseStudy/src/Appointment.java
+++ C206_CaseStudy/src/Appointment.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 /**
  * I declare that this code was written by me.
  * I will not copy or allow others to copy my code.
@@ -5,15 +7,18 @@
  * 19030130, 18 Aug 2020 11:44:56 am
  */
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 /**
  * @author 19030130
  *
  */
+//Done by Syakir
 public class Appointment {
 	
 	private String customerName;
-	private String appointmentDate;
-	private String appointmentTime;
+	private LocalDate appointmentDate;
+	private LocalDateTime appointmentTime;
 	private String designerName;
 	private String address;
 	/**
@@ -23,7 +28,7 @@ public class Appointment {
 	 * @param designerName
 	 * @param address
 	 */
-	public Appointment(String customerName, String appointmentDate, String appointmentTime, String designerName,
+	public Appointment(String customerName, LocalDate appointmentDate, LocalDateTime appointmentTime, String designerName,
 			String address) {
 		this.customerName = customerName;
 		this.appointmentDate = appointmentDate;
@@ -46,25 +51,25 @@ public class Appointment {
 	/**
 	 * @return the appointmentDate
 	 */
-	public String getAppointmentDate() {
+	public LocalDate getAppointmentDate() {
 		return appointmentDate;
 	}
 	/**
 	 * @param appointmentDate the appointmentDate to set
 	 */
-	public void setAppointmentDate(String appointmentDate) {
+	public void setAppointmentDate(LocalDate appointmentDate) {
 		this.appointmentDate = appointmentDate;
 	}
 	/**
 	 * @return the appointmentTime
 	 */
-	public String getAppointmentTime() {
+	public LocalDateTime getAppointmentTime() {
 		return appointmentTime;
 	}
 	/**
 	 * @param appointmentTime the appointmentTime to set
 	 */
-	public void setAppointmentTime(String appointmentTime) {
+	public void setAppointmentTime(LocalDateTime appointmentTime) {
 		this.appointmentTime = appointmentTime;
 	}
 	/**
