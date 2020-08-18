79b2bb8a917ee59f267df3ff305d173caa824ff2
19009811 <19009811@Jerald-Laptop>
2020-08-18 15:08:56 +0800

    committing and pushing main method, updated user account portion

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -15,40 +15,39 @@ public class C206_CaseStudy {
 		LocalDateTime timeNow = LocalDateTime.now();
 
 		// if using when customer use a different format
-		//DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-		//String formattedDate = timeNow.format(formatter1);
+		// DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd-MM-yyyy
+		// HH:mm:ss");
+		// String formattedDate = timeNow.format(formatter1);
 
 		appointmentList.add(new Appointment("Jason", dateNow, timeNow, "Jerome", "Woodlands"));
 
-		int option = 0;
+		int option = Helper.readInt("Enter an option");
 
-		//Syakir
+		// Syakir
 		while (option != 6) {
 
-			C206_CaseStudy.menu();
+			menu();
 			option = Helper.readInt("Enter an option > ");
 
 			if (option == 1) {
-
+				UserAccount(accountList);
 			} else if (option == 2) {
 
-				
 			} else if (option == 3) {
 
-				
 			} else if (option == 4) {
 
-				
 			} else if (option == 5) {
-				
-			} else if (option == 6){
-				System.out.println("Bye!");
+
 			} else {
 				System.out.println("Invalid option");
+				menu();
 			}
+			option = Helper.readInt("Enter an option");
 		}
 	}
-	//Syakir
+
+	// Syakir
 	public static void menu() {
 		C206_CaseStudy.setHeader("RENOVATION ACE");
 		System.out.println("1. User Account");
@@ -65,13 +64,19 @@ public class C206_CaseStudy {
 	 * @param string
 	 */
 	private static void setHeader(String header) {
-			Helper.line(80, "-");
-			System.out.println(header);
-			Helper.line(80, "-");
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
 
 	}
-	private static void UserAccount(ArrayList<UserAccount>accountList) {
-		
+
+	private static void UserAccount(ArrayList<UserAccount> accountList) {
+		System.out.println("USER ACCOUNT");
+		System.out.println("1. Add user account");
+		System.out.println("2. View all users");
+		System.out.println("3. Delete user by name");
+
+		int option = 0;
 	}
 
 }
