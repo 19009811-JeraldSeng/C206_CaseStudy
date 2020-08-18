5da131d6ae9110ebad1cbff3e237dd04e0b7f5af
19030130 <19030130@DESKTOP-483ETFT>
2020-08-18 13:49:53 +0800

    Case Study ArrayList for Appointment

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,21 @@
+import java.util.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Syakir
+		ArrayList<Appointment> appointmentList = new ArrayList<Appointment>();
+		DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		
+		
+		appointmentList.add(new Appointment("Jason", null, null, "Jerome", "Woodlands"));
 
+		
 	}
 
 }
