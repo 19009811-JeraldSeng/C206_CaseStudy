8410fc375c376b4a53ad6f3a69d28d85b6e48e75
19009811 <19009811@Jerald-Laptop>
2020-08-23 14:27:37 +0800

    pushing user account class

--- C206_CaseStudy/src/UserAccount.java
+++ C206_CaseStudy/src/UserAccount.java
@@ -22,36 +22,36 @@ public class UserAccount {
 		this.status = "new";
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
+	public String getName() {
+		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public String getRole() {
-		return role;
-	}
-
 	public String getEmail() {
 		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getStatus() {
 		return status;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
 	}
 
 	public void setRole(String role) {
