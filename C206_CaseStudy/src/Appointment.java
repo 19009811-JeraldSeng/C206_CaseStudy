fe9f125fdec4d190f44fedd294e706cc96cd165d
19030130 <19030130@DESKTOP-483ETFT>
2020-08-23 22:06:07 +0800

    Syakir's push for Appointment

--- C206_CaseStudy/src/Appointment.java
+++ C206_CaseStudy/src/Appointment.java
@@ -24,13 +24,6 @@ public class Appointment extends C206_CaseStudy {
 	private String designerName;
 	private String address;
 
-	/**
-	 * @param customerName
-	 * @param appointmentDate
-	 * @param timeNow
-	 * @param designerName
-	 * @param address
-	 */
 	public Appointment(String customerName, LocalDate appointmentDate, LocalTime timeNow, String designerName,
 			String address) {
 		this.customerName = customerName;
@@ -40,83 +33,55 @@ public class Appointment extends C206_CaseStudy {
 		this.address = address;
 	}
 
-	/**
-	 * @param customerName2
-	 * @param string
-	 * @param string2
-	 * @param designerName2
-	 * @param address2
-	 */
+
 	public Appointment(String customerName2, String string, String string2, String designerName2, String address2) {
-		// TODO Auto-generated constructor stub
+
 	}
 
-	/**
-	 * @return the customerName
-	 */
+
 	public String getCustomerName() {
 		return customerName;
 	}
 
-	/**
-	 * @param customerName the customerName to set
-	 */
+
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
 
-	/**
-	 * @return the appointmentDate
-	 */
+
 	public LocalDate getAppointmentDate() {
 		return appointmentDate;
 	}
 
-	/**
-	 * @param appointmentDate the appointmentDate to set
-	 */
+
 	public void setAppointmentDate(LocalDate appointmentDate) {
 		this.appointmentDate = appointmentDate;
 	}
 
-	/**
-	 * @return the appointmentTime
-	 */
+
 	public LocalTime getAppointmentTime() {
 		return appointmentTime;
 	}
 
-	/**
-	 * @param appointmentTime the appointmentTime to set
-	 */
+
 	public void setAppointmentTime(LocalTime appointmentTime) {
 		this.appointmentTime = appointmentTime;
 	}
 
-	/**
-	 * @return the designerName
-	 */
+
 	public String getDesignerName() {
 		return designerName;
 	}
 
-	/**
-	 * @param designerName the designerName to set
-	 */
+
 	public void setDesignerName(String designerName) {
 		this.designerName = designerName;
 	}
 
-	/**
-	 * @return the address
-	 */
 	public String getAddress() {
 		return address;
 	}
 
-	/**
-	 * @param address the address to set
-	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
